03c360ad84ccc552b7389d0089275397d88ee731
david <davidLiao@hahow.in>
2023-09-18 11:26:02 +0800

    feat: design system

---
+++ build.js
@@ -0,0 +1,64 @@
+// build.ts
+const { registerTransforms } = require('@tokens-studio/sd-transforms');
+const StyleDictionary = require('style-dictionary');
+
+registerTransforms(StyleDictionary);
+
+const myStyleDictionary = StyleDictionary.extend({
+  source: ['tokens/**/*transformed.json'],
+  platforms: {
+    css: {
+      transforms: [
+        'ts/descriptionToComment',
+        'ts/size/px',
+        'ts/typography/fontWeight',
+        'ts/resolveMath',
+        'ts/size/css/letterspacing',
+        'ts/typography/css/fontFamily',
+        'ts/typography/css/shorthand',
+        'ts/border/css/shorthand',
+        'ts/shadow/css/shorthand',
+        'ts/color/modifiers',
+        'name/cti/kebab',
+        'ts/shadow/css/shorthand',
+        'ts/typography/css/shorthand',
+      ],
+      buildPath: 'build/test/',
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+    cssDeep: {
+      transforms: [
+        'ts/descriptionToComment',
+        'ts/size/px',
+        'ts/typography/fontWeight',
+        'ts/resolveMath',
+        'ts/size/css/letterspacing',
+        'ts/typography/css/fontFamily',
+        'ts/typography/css/shorthand',
+        'ts/border/css/shorthand',
+        'ts/shadow/css/shorthand',
+        'ts/color/modifiers',
+        'name/cti/kebab',
+        'ts/shadow/css/shorthand',
+        'ts/typography/css/shorthand',
+      ],
+      buildPath: 'build/testDeep/',
+      files: [
+        {
+          destination: 'variables.json',
+          format: 'json/nested',
+          options: {
+            outputReferences: false,
+          },
+        },
+      ],
+    },
+  },
+});
+
+myStyleDictionary.buildAllPlatforms();
